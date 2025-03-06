7201c60805746b335afa748e7a213b593d545ef1
inevitablegs <inevitable3101@gmail.com>
2025-03-06 20:28:30 +0530

    updated req and pass

--- hiring_portal/settings.py
+++ hiring_portal/settings.py
@@ -151,4 +151,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'insightai.hackathonclub@gmail.com'  
-EMAIL_HOST_PASSWORD = 'rfpn qirj spih hlpv'  
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
